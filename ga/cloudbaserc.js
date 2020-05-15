ea2103e5d9a9939557e41b8b85f4a5b1c3c4e621
xuedaobian <1689359591@qq.com>
2020-05-15 20:36:35 +0800

    切换为tcb

---
+++ ga/cloudbaserc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    envId: "dev-test-0fyhq",
+    functionRoot: "./functions",
+    functions: [
+        {
+            name: "app",
+            // 超时时间
+            timeout: 5,
+            // 环境变量
+            envVariables: {},
+            runtime: "Nodejs10.15",
+            // 内存 128
+            memorySize: 128,
+            handler: "index.main"
+        }
+    ]
+};
