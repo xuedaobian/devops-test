632d3e6a55f45fe68d5db6ef6113163b8be56e97
xuedaobian <1689359591@qq.com>
2020-05-28 17:01:55 +0800

    增量部署1

--- ga/deploy.sh
+++ ga/deploy.sh
@@ -1,15 +1,15 @@
 cd ./functions
-echo '开始执行ls.sh文件'
+echo '开始执行部署操作'
 newlog=`eval "git rev-parse --short HEAD"`
 for dir in $(ls ./)
 do
     if [ -d $dir ] 
     then
-        echo $dir
         oldlog=`eval "git log --pretty=format:“%h” -1 $dir"`
         if [ $newlog = ${oldlog:1:7} ]
         then
             cd ..
+            echo "$dir 函数开始部署"
             # eval 'tcb functions:code:update $dir'
             eval 'tcb functions:deploy $dir --force' && echo "$dir 函数部署成功"
             cd ./functions
